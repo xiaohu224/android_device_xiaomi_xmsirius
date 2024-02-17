19bc80dd3cc1a7b3326a07805c04c705891c606a
Arian <arian.kulmer@web.de>
2024-02-17 11:47:35 +0100

    xmsirius: Build fingerprint extension interfaces from source
    Change-Id: I08db4948ec1d4b3df5f0c57f87c00575ab3fe778

--- device.mk
+++ device.mk
@@ -22,7 +22,12 @@ TARGET_SCREEN_WIDTH := 1080
 # Camera
 PRODUCT_PACKAGES += \
     libpiex_shim
-
+    
+# Fingerprint
+PRODUCT_PACKAGES += \
+    com.fingerprints.extension@1.0.vendor \
+    vendor.goodix.hardware.fingerprintextension@1.0.vendor
+    
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/idc/uinput-fpc.idc:system/usr/idc/uinput-fpc.idc \
