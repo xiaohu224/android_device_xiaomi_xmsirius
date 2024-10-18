a85243973e34c2fa90d478f474ac280bf493397f
LuK1337 <priv.luk@gmail.com>
2024-10-18 11:40:33 +0200

    xmsirius: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38

--- lineage_xmsirius.mk
+++ lineage_xmsirius.mk
@@ -23,6 +23,5 @@ PRODUCT_MODEL := Mi 8 SE
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="sirius-user 10 QKQ1.190828.002 V12.5.1.0.QEBCNXM release-keys"
-
-BUILD_FINGERPRINT := Xiaomi/sirius/sirius:10/QKQ1.190828.002/V12.5.1.0.QEBCNXM:user/release-keys
+    BuildDesc="sirius-user 10 QKQ1.190828.002 V12.5.1.0.QEBCNXM release-keys" \
+    BuildFingerprint=Xiaomi/sirius/sirius:10/QKQ1.190828.002/V12.5.1.0.QEBCNXM:user/release-keys
