a3be833ed1611a7219cfdbb0e51930f60b2893c8
Rocky7842 <eric.rocky7842@gmail.com>
2025-01-21 22:55:04 +0800

    xmsirius: Use libprotobuf-cpp v29 VNDK prebuilts from hardware/lineage/compat
    Change-Id: I846a3a2c4d4aa1ae226bd750e6b41a1e4f1b8623

--- extract-files.py
+++ extract-files.py
@@ -38,8 +38,6 @@ lib_fixups: lib_fixups_user_type = {
 blob_fixups: blob_fixups_user_type = {
     ('vendor/lib/libarcsoft_beautyshot.so', 'vendor/lib/libarcsoft_dualcam_optical_zoom_control.so'): blob_fixup()
         .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
-    ('vendor/lib/hw/camera.qcom.so', 'vendor/lib64/libsensorcal.so', 'vendor/lib64/sensors.ssc.so'): blob_fixup()
-        .replace_needed('libprotobuf-cpp-full.so', 'libprotobuf-cpp-full-v29.so'),
     'vendor/lib/camera/components/com.qti.node.watermark.so': blob_fixup()
         .add_needed('libpiex_shim.so'),
 }  # fmt: skip
